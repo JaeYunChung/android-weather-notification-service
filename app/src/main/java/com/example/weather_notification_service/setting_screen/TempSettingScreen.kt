3b4761861fd23f6e12c5b99d4790c1fd936b8f61
JaeYunChung <jy8532@ajou.ac.kr>
2025-06-06 10:58:43 +0900

    append time control ui

--- app/src/main/java/com/example/weather_notification_service/setting_screen/TempSettingScreen.kt
+++ app/src/main/java/com/example/weather_notification_service/setting_screen/TempSettingScreen.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
